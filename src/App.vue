7b121d493571345a46c06b39f013be55ae2dea25
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-06 16:10:54 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+  <script >
+  </script>
+
+  <template>
+    <header></header>
+    <main>
+
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+    </main>
+    
+    <footer></footer>
+  
+  </template>
+
+  <style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+  </style>
