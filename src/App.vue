cc88c6448ef5b67c6aa59807618c804636709021
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-06 16:52:14 +0200

    presentation of the navbar

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,22 @@
   </script>
 
   <template>
-    <header></header>
+    
     <main>
 
-      <section>
-
+      <section class="home">
+        <header class="border-b-2 h-[80px] bg-[#f8f0e2]">
+      <div class="flex justify-center items-center gap-28"> <!-- Center the div -->
+        <p class="text-[50px] text-orange-600 font-bold mt-[-30px] font-family-">R<span class="text-black">ufaro</span> </p>
+        <ul class="flex gap-8 ml-8"> <!-- Add space between list items -->
+          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Home</li>
+          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Skills</li>
+          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Projects</li>
+          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Resume</li>
+          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Contact</li>
+        </ul>
+      </div>
+    </header>
       </section>
       <section>
 
@@ -18,22 +29,18 @@
 
       </section>
     </main>
-    
+
     <footer></footer>
   
   </template>
 
   <style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  *{
+    margin: 10px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+
+  .home {
+    height: 100vh;
+    background-color: #f8f0e2;
   }
   </style>
