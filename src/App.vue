0c8d135c214bf2207f96585918c270bf7c7db6f6
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-06 17:15:09 +0200

    implemented the introductory text, button, social and image layout

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,43 @@
 
       <section class="home">
         <header class="border-b-2 h-[80px] bg-[#f8f0e2]">
-      <div class="flex justify-center items-center gap-28"> <!-- Center the div -->
-        <p class="text-[50px] text-orange-600 font-bold mt-[-30px] font-family-">R<span class="text-black">ufaro</span> </p>
-        <ul class="flex gap-8 ml-8"> <!-- Add space between list items -->
-          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Home</li>
-          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Skills</li>
-          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Projects</li>
-          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Resume</li>
-          <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Contact</li>
-        </ul>
-      </div>
-    </header>
+          <div class="flex justify-center items-center gap-28"> <!-- Center the div -->
+            <p class="text-[50px] text-orange-600 font-bold mt-[-30px] font-family-">R<span class="text-black">ufaro</span> </p>
+            <ul class="flex gap-8 ml-8"> <!-- Add space between list items -->
+              <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Home</li>
+              <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Skills</li>
+              <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Projects</li>
+              <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Resume</li>
+              <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Contact</li>
+            </ul>
+          </div>
+        </header>
+
+        <div class="flex justify-center mt-[60px]">
+          <div class="w-[700px] mt-[60px]">
+            <p class="absolute mt-[5px]"><span class="text-orange-600  pt-[-60px]">*</span>Web developer</p>
+            <p class="text-[80px] font-bold">Hello! I am Rufaro Mandipa</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores fugit sequi iure ipsam sed temporibus! Odio magnam accusamus eos quia, facere soluta expedita laudantium dolorem, pariatur voluptates quod iste aspernatur!</p>
+
+            <div class="">
+              <button class="h-[50px] w-[120px] bg-orange-600">Lets talk</button>
+              <button class="h-[50px] w-[120px] bg-orange-600">View projects</button>
+
+            </div>
+
+            <div class="flex">
+              <p>Check out my __________</p>
+            <img src="" alt="soc1">
+            <img src="" alt="soc1">
+            <img src="" alt="soc1">
+            </div>
+          </div>
+
+          <div class="w-[700px] h-[800px] bg-blue-200">
+
+          </div>
+        </div>
+
       </section>
       <section>
 
