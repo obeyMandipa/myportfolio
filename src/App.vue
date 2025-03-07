0337b597bb887da22b67fe59590cc5aca5922f8b
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-07 18:32:44 +0200

    added the elements to add my skills, and set up the projects pages

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <section class="home">
         <header class="border-b-2 h-[80px] bg-[#f8f0e2]">
           <div class="flex justify-center items-center gap-28"> <!-- Center the div -->
-            <p class="text-[50px] text-orange-600 font-bold mt-[-30px] font-family-">R<span class="text-black">ufaro</span> </p>
+            <p class="text-[50px] text-orange-600 font-bold mt-[-30px]">R<span class="text-black">ufaro</span> </p>
             <ul class="flex gap-8 ml-8"> <!-- Add space between list items -->
               <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Home</li>
               <li class="text-[19px] h-[60px] border-b-2 hover:text-orange-600 hover:border-orange-600 cursor-pointer">Skills</li>
@@ -19,11 +19,11 @@
           </div>
         </header>
 
-        <div class="flex justify-center mt-[60px]">
+        <div class="flex justify-center mt-[60px] ">
           <div class="w-[700px] mt-[60px]">
             <p class="absolute mt-[5px]"><span class="text-orange-600  pt-[-60px]">*</span>Web developer</p>
-            <p class="text-[80px] font-bold">Hello! I am Rufaro Mandipa</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores fugit sequi iure ipsam sed temporibus! Odio magnam accusamus eos quia, facere soluta expedita laudantium dolorem, pariatur voluptates quod iste aspernatur!</p>
+            <p class="text-[80px] font-bold ">Hello! I am Rufaro Mandipa</p>
+            <p class="w-[550px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores fugit sequi iure ipsam sed temporibus! Odio magnam accusamus eos quia, facere soluta expedita laudantium dolorem, pariatur voluptates quod iste aspernatur!</p>
 
             <div class="">
               <button class="h-[50px] w-[120px] bg-orange-600">Lets talk</button>
@@ -45,12 +45,30 @@
         </div>
 
       </section>
-      <section>
 
-      </section>
-      <section>
+      <section class="skills ">
+        <div class="ml-[270px] mt-[60px]">
+          <p class="absolute mt-[-10px]"><span class="text-orange-600  pt-[-60px]">*</span>Skills</p>
+          <p class="text-[60px] font-bold ">What i can do for you!</p>
+          <p class="w-[550px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, nihil. Quos non maiores, nostrum est molestias amet quaerat ducimus natus praesentium tempore ex dolores asperiores dolorum dicta molestiae a quod.</p>
+        </div>
 
+        <div class="flex justify-center mt-[60px] space-x-[80px]">
+          <div class="w-[400px] h-[500px] border-orange-600  border-l-[5px] border-t-[5px] rounded-[20px] shadow-2xl"></div>
+          <div class="w-[400px] h-[500px] border-orange-600  border-l-[5px] border-b-[5px] rounded-[20px] shadow-2xl"></div>
+          <div class="w-[400px] h-[500px] border-orange-600  border-l-[5px] border-t-[5px] rounded-[20px] shadow-2xl"></div>
+        </div>
       </section>
+
+      <section class="projects">
+
+        <div class="absolute ml-[270px] mt-[80px]">
+          <p class="absolute mt-[-10px]"><span class="text-orange-600  pt-[-60px]">*</span>Projects</p>
+          <p class="text-[60px] font-bold ">What i have done!</p>
+        </div>
+        
+      </section>
+
       <section>
 
       </section>
@@ -69,4 +87,16 @@
     height: 100vh;
     background-color: #f8f0e2;
   }
+
+  .skills {
+    height: 100vh;
+    background-color: #ffffff;
+  }
+
+  .projects {
+    height: 100vh;
+    background-color: #f8f0e2;
+  }
+
+  
   </style>
